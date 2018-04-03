499c2425f62caba5f7dcfff1fed08d41d49d8380
Boris Li <boriskenli@gmail.com>
2018-04-04 09:11:35 +1000

    basic index.html with a modularised infrastructure

--- js/controls.js
+++ js/controls.js
@@ -1,22 +1,36 @@
 window.PLANET = window.PLANET || {};
 PLANET.controls = PLANET.controls || {};
 
+
 PLANET.controls.Controls = function() {
     controls = new THREE.OrbitControls(camera, renderer.domElement);
+
     //for stopping animations during user control
-    this.inControl = false;
-    window.addEventListener('mousedown', function(ev) {
-        this.inControl = true;
-    });
-    window.addEventListener('mouseup', function(ev) {
-        this.inControl = false;
-    });
-    window.addEventListener('resize', function(ev) {
+    inControl = false;
+    PLANET.controls.addMouseEventListener();
+
+    PLANET.controls.addResizeListener();
+};
+
+PLANET.controls.addResizeListener = function() {
+    window.addEventListener('resize', function() {
         var width = window.innerWidth;
         var height = window.innerHeight;
         renderer.setSize(width, height);
         camera.aspect = width / height;
         camera.updateProjectionMatrix();
-        renderer.render(scene, camera);
+    });
+};
+
+PLANET.controls.addMouseEventListener = function() {
+    window.addEventListener('mousedown', function(ev) {
+        inControl = true;
+        if(ev.button === 2) {
+            controls.reset();
+        }
+    });
+
+    window.addEventListener('mouseup', function() {
+        inControl = false;
     });
 };
--- js/main.js
+++ js/main.js
@@ -1,36 +1,52 @@
 window.PLANET = window.PLANET || {};
 PLANET.main = PLANET.main || {};
 
+//variables that need global access
+var scene, camera, renderer, light, cube, sphere, inControl;
+const CUBE_SIZE = 1, SPHERE_RADIUS = 60;
+
 PLANET.main.main = function() {
-    renderer = new THREE.WebGLRenderer();
-    // renderer.antialias = true;
-    // renderer.gammaInput = true;
-    // renderer.gammaOutput = true;
-    // renderer.setClearColor(0x000000, 1);
-    var width = window.innerWidth;
-    var height = window.innerHeight;
-    renderer.setSize(width, height);
-    // renderer.domElement.setAttribute('id', 'renderer');
-    document.body.appendChild(renderer.domElement);
-
+    //init scene
     scene = new THREE.Scene();
-    var aspect = width / height;
-    camera = new THREE.PerspectiveCamera(60, aspect, 0.1, 10000);
-    camera.position.set(0, 0, 120);
-
-    PLANET.main.test(scene);
-
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    window.addEventListener('resize', function() {
+        var width = window.innerWidth;
+        var height = window.innerHeight;
+        renderer.setSize(width, height);
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix();
+        renderer.render(scene, camera);
+    });
+    //init camera
+    var aspect = window.innerWidth / window.innerHeight;
+    camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 5000);
+    camera.position.set(0, 10, 70);
+    //init light
+    light = new THREE.DirectionalLight(0xffffff, 0.8);
+    light.position.set(-80, 80, 80);
+    scene.add(light);
+    PLANET.main.addObjects();
+    PLANET.controls.Controls();
     PLANET.main.render();
+};
+
+PLANET.main.addObjects = function() {
+    cube = new PLANET.cube.Cube(CUBE_SIZE);
+    scene.add(cube);
+    sphere = new PLANET.sphere.Sphere(SPHERE_RADIUS);
+    scene.add(sphere);
 };
 
 PLANET.main.render = function() {
     requestAnimationFrame(PLANET.main.render);
-    renderer.render(scene, camera);
-};
 
-PLANET.main.test = function(scene) {
-    cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-    cubeMaterial = new THREE.MeshNormalMaterial();
-    cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-    scene.add(cube);
+    if(!inControl) {
+        cube.rotation.x += 0.05;
+        cube.rotation.y += 0.05;
+        sphere.rotation.y -= 0.005;
+    }
+
+    renderer.render(scene, camera);
 };
