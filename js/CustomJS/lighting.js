1ba34fc7a9cdb083399e5dfc3ae84e538250fd33
Kyle Hammer <kylethomashammer@gmail.com>
2018-05-23 12:37:32 +1000

    Update to Sky Shaders
    Update

--- js/CustomJS/lighting.js
+++ js/CustomJS/lighting.js
@@ -1,6 +1,17 @@
 window.PLANET = window.PLANET || {};
 PLANET.lighting = PLANET.lighting || {};
 
+var effectController  = {
+  turbidity: 10,
+  rayleigh: 2,
+  mieCoefficient: 0.005,
+  mieDirectionalG: 0.8,
+  luminance: 1,
+  inclination: 0.49, // elevation / inclination
+  azimuth: 0.25, // Facing front,
+  sun:  true
+};
+
 PLANET.lighting.Lighting = function () {
     THREE.Object3D.call(this);
 
@@ -65,40 +76,15 @@ PLANET.lighting.Lighting = function () {
 				sunSphere.visible = true;
 				scene.add( sunSphere );
 				/// GUI
-				var effectController  = {
-					turbidity: 10,
-					rayleigh: 2,
-					mieCoefficient: 0.005,
-					mieDirectionalG: 0.8,
-					luminance: 1,
-					inclination: 0.49, // elevation / inclination
-					azimuth: 0.25, // Facing front,
-					sun:  true
-				};
-				var distance = 10000;
-				function guiChanged() {
-					var uniforms = sky.material.uniforms;
-					uniforms.turbidity.value = effectController.turbidity;
-					uniforms.rayleigh.value = effectController.rayleigh;
-					uniforms.luminance.value = effectController.luminance;
-					uniforms.mieCoefficient.value = effectController.mieCoefficient;
-					uniforms.mieDirectionalG.value = effectController.mieDirectionalG;
-					var theta = Math.PI * ( effectController.inclination - 0.5 );
-					var phi = 2 * Math.PI * ( effectController.azimuth - 0.5 );
-					sunSphere.position.x = distance * Math.cos( phi );
-					sunSphere.position.y = distance * Math.sin( phi ) * Math.sin( theta );
-					sunSphere.position.z = distance * Math.sin( phi ) * Math.cos( theta );
-					sunSphere.visible = effectController.sun;
-					uniforms.sunPosition.value.copy( sunSphere.position );
-					renderer.render( scene, camera );
-				}
+
+
 				var gui = new dat.GUI();
 				gui.add( effectController, "turbidity", 1.0, 20.0, 0.1 ).onChange( guiChanged );
 				gui.add( effectController, "rayleigh", 0.0, 4, 0.001 ).onChange( guiChanged );
 				gui.add( effectController, "mieCoefficient", 0.0, 0.1, 0.001 ).onChange( guiChanged );
 				gui.add( effectController, "mieDirectionalG", 0.0, 1, 0.001 ).onChange( guiChanged );
 				gui.add( effectController, "luminance", 0.0, 2 ).onChange( guiChanged );
-				gui.add( effectController, "inclination", 0, 1, 0.0001 ).onChange( guiChanged );
+				gui.add( effectController, "inclination", -1, 1, 0.0001 ).onChange( guiChanged );
 				gui.add( effectController, "azimuth", 0, 1, 0.0001 ).onChange( guiChanged );
 				gui.add( effectController, "sun" ).onChange( guiChanged );
 				guiChanged();
@@ -112,7 +98,24 @@ PLANET.lighting.Lighting = function () {
 PLANET.lighting.Lighting.prototype = Object.create(THREE.Object3D.prototype);
 
 
+function guiChanged() {
+  var distance = 10000;
 
+  var uniforms = sky.material.uniforms;
+  uniforms.turbidity.value = effectController.turbidity;
+  uniforms.rayleigh.value = effectController.rayleigh;
+  uniforms.luminance.value = effectController.luminance;
+  uniforms.mieCoefficient.value = effectController.mieCoefficient;
+  uniforms.mieDirectionalG.value = effectController.mieDirectionalG;
+  var theta = Math.PI * ( effectController.inclination - 0.5 );
+  var phi = 2 * Math.PI * ( effectController.azimuth - 0.5 );
+  sunSphere.position.x = distance * Math.cos( phi );
+  sunSphere.position.y = distance * Math.sin( phi ) * Math.sin( theta );
+  sunSphere.position.z = distance * Math.sin( phi ) * Math.cos( theta );
+  sunSphere.visible = effectController.sun;
+  uniforms.sunPosition.value.copy( sunSphere.position );
+  renderer.render( scene, camera );
+}
 
 
 /////////////////////////////////////////////////////
@@ -120,6 +123,11 @@ PLANET.lighting.Lighting.prototype = Object.create(THREE.Object3D.prototype);
 /////////////////////////////////////////////////////
 
 PLANET.lighting.animate = function () {
+  effectController.inclination += 0.01;
+  if (effectController.inclination > 1) {
+    effectController.inclination = -1;
+  }
+  guiChanged();
   //this.sunLight.position.x = sunRadius * Math.cos(timer);
   //this.sunLight.position.z = sunRadius * Math.sin(timer);
   //this.moonLight.position.x = moonRadius * -Math.cos(timer);
