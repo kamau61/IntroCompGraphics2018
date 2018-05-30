2eebb90ff4c9929ba70dbc0130e672d8d7555fb6
Isaac Buckton <isaac.d.buckton@student.uts.edu.au>
2018-05-30 10:59:10 +1000

    Added low-res model for debugging

--- js/CustomJS/main.js
+++ js/CustomJS/main.js
@@ -12,7 +12,7 @@ const CONSTANTS = {
 };
 let params = {
     PlanetRadius: 100,
-    PlanetDetail: 7,
+    PlanetDetail: 5,
     PlanetWireframe: false,
     PlanetFlatShading: true,
     PlanetRotationY: 0,
