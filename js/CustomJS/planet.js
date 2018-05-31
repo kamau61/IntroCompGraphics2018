68ea1b5aef33830d6a147b4c6378da82da3dece2
Boris Li <boriskenli@gmail.com>
2018-05-31 12:34:30 +1000

    lava test

--- js/CustomJS/planet.js
+++ js/CustomJS/planet.js
@@ -5,15 +5,15 @@ PLANET.planet.Planet = function(bufferGeometry) {
     THREE.Object3D.call(this);
     this.name = "Planet";
     simplex = new SimplexNoise();
-    this.ocean = PLANET.ocean.Ocean(bufferGeometry);
+    // this.ocean = PLANET.ocean.Ocean(bufferGeometry);
+    this.ocean = PLANET.lava.Lava(bufferGeometry);
     this.add(this.ocean);
     this.terrain = PLANET.terrain.Terrain(bufferGeometry);
-    this.add(this.terrain);
+    // this.add(this.terrain);
 
     this.climate = PLANET.climate.Climate();
     this.animate = function () {
         this.ocean.animate();
-        // this.climate.animate();
     };
     this.update = function () {
         this.terrain.update();
